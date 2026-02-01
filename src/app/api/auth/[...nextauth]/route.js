4ed2dc37c74604e1151d51bf4fc77509fe54a178
Ankur Goyal <ankurgoyal1227@gmail.com>
2026-02-01 13:59:57 +0530

    simplify auth: remove adapter and provider linking

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,25 +1,53 @@
+// import NextAuth from "next-auth";
+// import GithubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
+// import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+// import clientPromise from "../../../../../lib/mongodb";
+// const handler = NextAuth({
+//   adapter: MongoDBAdapter(clientPromise),
+
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//       // allowDangerousEmailAccountLinking: true,
+//     }),
+
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       // allowDangerousEmailAccountLinking: true,
+//     }),
+//   ],
+// });
+
+// export { handler as GET, handler as POST };
+
+
+
+
+
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "../../../../../lib/mongodb";
-const handler = NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
 
+const handler = NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-
-      // 🔥 YE LINE MUST HAI
-      allowDangerousEmailAccountLinking: true,
     }),
-
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
 
+   callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true; 
+    },
+  },
+});
 export { handler as GET, handler as POST };
