8d63165f49de864a76c53e07b3d1f4fb2ccee95c
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-03-16 02:34:13 +0530

    Update page.js

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -26,7 +26,7 @@ const menu = [
 
  async function fet() {
   sl("loading your data....")
-              const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/users?email=${session?.user?.email}`);
+              const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/complaint/users?email=${session?.user?.email}`);
 const data = await res.json();
 if(!data.length){
   sl("Not found....")
